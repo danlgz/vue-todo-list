b3289dc29f84646b998078d794d1ec5068b400d1
Daniel Lucas <danielalejandro137@gmail.com>
2020-05-24 23:55:42 -0600

    REF script yarn

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+yarn build
 
 # navigate into the build output directory
 cd dist
@@ -20,6 +20,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+# git push -f git@github.com:danlgz/vue-todo-list.git master:gh-pages
 cd -
